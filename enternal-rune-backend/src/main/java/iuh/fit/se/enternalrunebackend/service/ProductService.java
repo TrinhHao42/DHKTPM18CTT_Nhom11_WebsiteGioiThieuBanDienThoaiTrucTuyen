85ad1c26be692499ef02541f1c17c0409015487b
buikhanhlan1410 <buikhanhlan1410@gmail.com>
2025-11-28 16:04:58 +0700

    feat: update cloudinary

--- enternal-rune-backend/src/main/java/iuh/fit/se/enternalrunebackend/service/ProductService.java
+++ enternal-rune-backend/src/main/java/iuh/fit/se/enternalrunebackend/service/ProductService.java
@@ -10,7 +10,9 @@ import iuh.fit.se.enternalrunebackend.entity.Product;
 import iuh.fit.se.enternalrunebackend.entity.enums.ProductStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ProductService {
@@ -35,7 +37,7 @@ public interface ProductService {
             String keyword, String brand, ProductStatus status, Pageable pageable
     );
 
-    public void  addProduct(ProductRequest productRequest);
+    public void  addProduct(ProductRequest productRequest,List<MultipartFile> files)throws IOException;
     public void deleteProduct(Integer id);
     public Product updateProduct(Integer productId, ProductRequest request);
 
